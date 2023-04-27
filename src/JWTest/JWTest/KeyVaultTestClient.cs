9db47f0086e8d8d301c879e60ab1f58d53dbd189
Junbo Wang <juniwang@microsoft.com>
2023-04-27 13:46:48 +0800

    add kv test

---
+++ src/JWTest/JWTest/KeyVaultTestClient.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace JWTest
+{
+    internal class KeyVaultTestClient
+    {
+        public async Task Test()
+        {
+            var uri = new Uri(Environment.GetEnvironmentVariable("kvBasUri"));
+            var client = new SecretClient(uri, GetCredential());
+
+            while (true)
+            {
+                try
+                {
+                    var secret = await client.GetSecretAsync("test");
+                    await Console.Out.WriteLineAsync("CurrentValue: " + secret.Value.Value);
+                    await client.SetSecretAsync("test", DateTime.UtcNow.ToLongTimeString());
+                }
+                catch (Exception e)
+                {
+                    await Console.Out.WriteLineAsync(e.Message);
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        private TokenCredential GetCredential()
+        {
+            return new WorkloadIdentityCredential();
+        }
+    }
+}
